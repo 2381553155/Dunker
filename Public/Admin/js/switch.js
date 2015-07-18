40b3becd02b8009c9e7592fd3948a8ff8d371e94
WP <2381553155@qq.com>
2015-07-18 15:45:13 +0800

    Revert "蹲坑添加项目"
    This reverts commit 22c4f6b8b58fde03dd75e3db4f3ed1cf4947c574.

--- Public/Admin/js/switch.js
+++
@@ -1,9 +0,0 @@
-($(document).ready(function(){
-    
-    "use strict";
-    
-    $(".btn-group").on("click", function(){
-        $(this).find(".btn").toggleClass("select").toggleClass("unselect");  
-    });
-
-}))(jQuery);
